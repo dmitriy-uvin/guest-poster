4c98a80f7cfa317dfcbc92dfb1028c6fe6b832a0
dmitriy-sanders <dimauuvin@gmail.com>
2020-09-14 18:42:56 +0300

    Add migration for users

--- api/database/migrations/2014_10_12_000000_create_users_table.php
+++ api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Constants\UserRoles;
 
 class CreateUsersTable extends Migration
 {
@@ -19,6 +20,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('skype')->nullable();
+            $table->string('website')->nullable();
+            $table->string('role')->default(UserRoles::USER);
+            $table->boolean('subscription_active')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
