5681f8f9120ae524dd06929efb0743c45fdd48f4
dmitriy-sanders <dimauuvin@gmail.com>
2020-09-25 18:21:12 +0300

    Add page for orders and Profile

---
+++ client/src/views/Orders.vue
@@ -0,0 +1,31 @@
+<template>
+    <AdminOrders v-if="isAdmin"/>
+    <UserOrders v-else />
+</template>
+
+<script>
+import AdminOrders from '@/components/orders/AdminOrders';
+import UserOrders from '@/components/orders/UserOrders';
+import { mapGetters } from 'vuex';
+import * as getters from '@/store/modules/user/types/getters';
+
+export default {
+    name: 'Orders',
+    components: {
+        AdminOrders,
+        UserOrders
+    },
+    computed: {
+        ...mapGetters('user', {
+            user: getters.GET_LOGGED_USER
+        }),
+        isAdmin() {
+            return this.user.role === 'admin';
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
