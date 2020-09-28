34cafbe6a4f2ad033142eb96fb01019a152e0bad
dmitriy-sanders <dimauuvin@gmail.com>
2020-09-28 16:28:56 +0300

    Add format mixin

---
+++ client/src/mixins/valueFormatMixin.js
@@ -0,0 +1,12 @@
+import moment from 'moment';
+
+export default {
+    methods: {
+        createdAt(date) {
+            return moment(date).format('DD MMMM Y') + " | " + moment(date).format('kk:mm');
+        },
+        totalPrice(price) {
+            return price.toFixed(2).toString().replace('.', ',');
+        }
+    }
+}
