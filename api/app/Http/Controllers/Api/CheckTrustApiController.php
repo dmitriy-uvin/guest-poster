ed2370804939cf339f2e270bae25a0bd668e6525
Dmitriy Uvin <dimauuvin@gmail.com>
2020-10-12 18:17:57 +0300

    Add controller for CheckTrust

---
+++ api/app/Http/Controllers/Api/CheckTrustApiController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class CheckTrustApiController extends ApiController
+{
+    private string $checkTrustApiKey;
+
+    public function __construct()
+    {
+        $this->checkTrustApiKey = env('CHECK_TRUST_API_KEY');
+    }
+
+    public function getCheckTrustData(Request $request)
+    {
+        $params = [
+            'trust',
+            'spam',
+            'lrtPowerTrust'
+        ];
+        $paramsString = implode(',', $params);
+
+        $url = 'https://checktrust.ru/app.php?r=host/app/summary/basic&applicationKey=' .
+            $this->checkTrustApiKey . '&host=' .
+            $request->query('host') . '&parameterList=' . $paramsString;
+
+        $response = Http::get($url);
+
+        return $response;
+    }
+}
