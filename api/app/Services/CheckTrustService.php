00247c121120ec056fe101ebdc512cebc456dffb
Dmitriy Uvin <dimauuvin@gmail.com>
2020-10-19 13:41:28 +0300

    Add CheckTrust api service

--- api/app/Services/CheckTrustService.php
+++ api/app/Services/CheckTrustService.php
@@ -4,7 +4,26 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Http;
+
 final class CheckTrustService
 {
+    private string $checkTrustApiEndpoint = 'https://checktrust.ru/app.php?r=host/app/summary/basic&applicationKey=';
+    private string $checkTrustApiKey = 'a0aece19412bc5f00b4e595fbd158fb4';
+    private array $params = [
+        'trust',
+        'spam',
+        'lrtPowerTrust'
+    ];
 
+    public function getDataFromApiByPlatform(string $platform)
+    {
+        $paramsString = implode(',', $this->params);
+
+        $url = $this->checkTrustApiEndpoint .
+            $this->checkTrustApiKey . '&host=' .
+            $platform . '&parameterList=' . $paramsString;
+
+        return Http::get($url)->object();
+    }
 }
