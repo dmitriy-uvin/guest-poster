fe79feca6e50c3222d2ae97e26473fc7142b44fc
Dmitriy Uvin <dimauuvin@gmail.com>
2020-10-30 17:59:31 +0200

    Update jobs

--- api/app/Action/Platform/UpdateApiDataByIdsAction.php
+++ api/app/Action/Platform/UpdateApiDataByIdsAction.php
@@ -6,13 +6,17 @@ namespace App\Action\Platform;
 
 use App\Action\ByIdsRequest;
 use App\Jobs\UpdateApiDataByPlatformIdJob;
+use App\Models\Constants\JobConstants;
 use App\Models\Platform;
 
 final class UpdateApiDataByIdsAction
 {
     public function execute(ByIdsRequest $request)
     {
-        $platformsChunks = collect(Platform::whereIn('id', $request->getIds())->get()->all())->chunk(15);
+        $platformsChunks = collect(Platform::whereIn('id', $request->getIds())
+            ->get()
+            ->all())
+            ->chunk(JobConstants::CHUNK_SIZE);
         foreach ($platformsChunks as $chunk) {
             UpdateApiDataByPlatformIdJob::dispatch($chunk);
         }
